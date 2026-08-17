0e06366d2c7caa7852f5639edfc275de7801fe2b
Avanish Vishwakarma <115359754+vishavanish@users.noreply.github.com>
2026-08-17 21:30:09 +0530

    Refactor moveZero function implementation
    Refactor moveZero function to use a while loop for better clarity and performance.

---
+++ moveZero.py
@@ -0,0 +1,27 @@
+def moveZero(nums):
+    
+    right = 0
+    for i in range(len(nums)):
+        if nums[i] != 0:
+            nums[right], nums[i] = nums[i], nums[right]
+            right += 1
+
+    
+    print(nums)
+
+nums = [0, 1, 0, 3, 0]
+moveZero(nums)
+
+#==============================================================
+def moveZero(nums):
+    left  =0
+    right = 0
+    while right < len(nums):
+        if nums[right] != 0:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+        right += 1
+    print(nums)
+
+nums = [0, 1, 0, 3, 0]
+moveZero(nums)
